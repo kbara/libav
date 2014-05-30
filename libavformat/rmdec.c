6b7351d71570fe989fa05cda7a67ed62fb481e1a
Katerina Barone-Adesi <katerinab+libav@gmail.com>
2014-05-30 20:06:53 +0200

    Towards metadata parsing; the wiki and file do not match

--- libavformat/rmdec.c
+++ libavformat/rmdec.c
@@ -22,6 +22,11 @@
 /* Format documentation:
  * http://wiki.multimedia.cx/index.php?title=RealMedia
  * https://common.helixcommunity.org/2003/HCS_SDK_r5/htmfiles/rmff.htm
+ *
+ * Naming convention:
+ * ra_ = RealAudio
+ * rm_ = RealMedia (potentially with video)
+ * real_ = both.
  */
 
 #include "libavutil/channel_layout.h"
@@ -32,7 +37,10 @@
 
 /* Header for RealAudio 1.0 (.ra version 3
  * and RealAudio 2.0 file (.ra version 4). */
-#define RA_HEADER MKTAG('.', 'r', 'a', 0xfd)
+//#define RA_HEADER MKTAG('.', 'r', 'a', 0xfd)
+//#define REAL_CONTENT_DESCRIPTION MKTAG('C', 'O', 'N', 'T')
+#define RA_HEADER ".ra\xfd"
+#define REAL_CONTENT_DESCRIPTION "CONT"
 
 /* The relevant VSELP format has 159-bit frames, stored in 20 bytes */
 #define RA144_PKT_SIZE 20
@@ -48,43 +56,136 @@ typedef struct {
 typedef struct {
 } RMDemuxContext;
 
+/* Return value >= 0: bytes read.
+ * Return value < 0: error.
+ */
+static int read_content_description_field(AVFormatContext *s, const char *desc)
+{
+    AVIOContext *acpb = s->pb;
+    uint16_t len;
+    uint8_t *val;
+    len = avio_r8(acpb);
+    val = av_mallocz(len + 1);
+    if (!val)
+        return AVERROR(ENOMEM);
+    avio_read(acpb, val, len);
+    printf("Hm: %i, %s\n", len+2, val);
+    return len + 2;
+}
+
+/* The content description header is documented, and the same in RA and RM:
+ * https://common.helixcommunity.org/2003/HCS_SDK_r5/htmfiles/rmff.htm
+ * It is similar for RA, but CONT is not set.
+ */
+
+static int real_read_content_description(AVFormatContext *s)
+{
+    AVIOContext *acpb = s->pb;
+    //uint32_t cdh_size, sought;
+    //uint16_t object_version; //, title_len, author_len, copyright_len, comment_len;
+    //uint8_t *title, *author, *copyright, *comment;
+    //char object_id[4];
+    uint16_t data_size;
+    int sought;
+    int tmp;
+    /* The RM spec defines these 10 bytes; RA files don't have the same opinion.
+    //object_id = avio_rb32(acpb);
+    avio_read(acpb, object_id, 4);
+    //if (object_id != REAL_CONTENT_DESCRIPTION) { 
+    if ((!isRA) && memcmp(object_id, REAL_CONTENT_DESCRIPTION, 4)) {
+        printf("%hhx%hhx%hhx%hhx vs %c%c%c%c\n", object_id[0], object_id[1], object_id[2], object_id[3], REAL_CONTENT_DESCRIPTION[0], REAL_CONTENT_DESCRIPTION[1], REAL_CONTENT_DESCRIPTION[2], REAL_CONTENT_DESCRIPTION[3]);
+        av_dlog(s,
+                "Invalid Content Description Header: expected ..., got ...\n");
+        return AVERROR_INVALIDDATA;
+    }
+    cdh_size = avio_rb32(acpb);
+    printf("Got size %i\n", cdh_size);
+    object_version = avio_rb16(acpb);
+    printf("Got object_version %i\n", object_version);
+    if (object_version != 0) {
+        av_dlog(s,
+                "Bad version %s in a Content Description Header, should be 0.\n",
+                object_version);
+        return AVERROR_INVALIDDATA;
+    } */
+    /* The wiki claims 10 unknown, then dword data size.
+     * A real file suggests it's 13 unknown, then byte data size.
+     */
+    avio_skip(acpb, 13);
+    data_size = avio_r8(acpb);
+    sought = 12; /* Header bytes read so far */
+    /*
+    title_len = avio_rb16(acpb);
+    author_len = avio_rb16(acpb);
+    _len = avio_rb16(acpb);
+    title_len = avio_rb16(acpb);
+    */
+
+    tmp = read_content_description_field(s, "title");
+    if (tmp < 0)
+        return tmp;
+    else
+        sought += tmp;
+    tmp = read_content_description_field(s, "author");
+    if (tmp < 0)
+        return tmp;
+    else
+        sought += tmp;
+    tmp = read_content_description_field(s, "copyright");
+    if (tmp < 0)
+        return tmp;
+    else
+        sought += tmp;
+    tmp = read_content_description_field(s, "comment");
+    if (tmp < 0)
+        return tmp;
+    else
+        sought += tmp;
+
+    printf("sought: %i, data_size: %i\n", sought, data_size);
+    if (sought != data_size + 12) {
+        printf("d'oh\n");
+        av_dlog(s,
+                "Content Description Header size declared %s, was %s.\n",
+                cdh_size, sought);
+        return AVERROR_INVALIDDATA;
+    }
+    return sought;
+}
+
+
 static int ra_probe(AVProbeData *p)
 {
-    /* RealAudio header; TODO: handle RMF later. */
-    /* TODO: check that this works */
+    /* RealAudio header; for RMF, use rm_probe */
     /* TODO: also check the version */
-    uint32_t buftag = MKTAG(p->buf[0], p->buf[1], p->buf[2], p->buf[3]);
-    return (buftag == RA_HEADER) ? AVPROBE_SCORE_MAX : 0;
+    //uint32_t buftag = MKTAG(p->buf[0], p->buf[1], p->buf[2], p->buf[3]);
+    return (!memcmp(p->buf, RA_HEADER, 4)) ? AVPROBE_SCORE_MAX : 0;
 }
 
 static int ra_read_header(AVFormatContext *s)
 {
     AVIOContext *acpb = s->pb;
-    //RMDemuxContext *rmdc = s->priv_data;
     AVStream *st = NULL;
 
-    uint32_t tag;
+    char tag[4];
+    //uint32_t tag;
     uint16_t version, header_size;
-    char a, b, c, d;
+    int tmp;
 
-    tag = avio_rl32(acpb);
+    //tag = avio_rb32(acpb);
+    avio_read(acpb, tag, 4);
 
-    a = tag >> 24;
-    b = (tag >> 16) & 0xff;
-    c = (tag >> 8) & 0xff;
-    d = tag & 0xff;
-    printf("Got tag %c%c%c%c\n", a, b, c, d);
-    printf("tag %u, header %u\n", tag, RA_HEADER);
-    if (tag != RA_HEADER)
+    //if (tag != RA_HEADER)
+    if (memcmp(tag, RA_HEADER, 4))
         return AVERROR_INVALIDDATA;
     version = avio_rb16(acpb);
-    printf("Version: %u\n", version);
-    if (version != 3) /* TODO: add v4 uspport */
+    if (version != 3) /* TODO: add v4 support */
         return AVERROR_INVALIDDATA;
     header_size = avio_rb16(acpb);
-    printf("Header size: %u\n", header_size);
-    avio_skip(acpb, header_size); /* TODO: read rest of header properly */
+    //avio_skip(acpb, header_size); /* TODO: read rest of header properly */
     /* TODO: make sure metadata round-trips, for example to AAC/mp4 */
+    tmp = real_read_content_description(s);
+    printf("Got tmp %i\n", tmp);
 
     st = avformat_new_stream(s, NULL);
     if (!st)
@@ -123,7 +224,6 @@ static int rm_probe(AVProbeData *p)
 
 static int rm_read_header(AVFormatContext *s)
 {
-    printf("rm_read_header called...\n");
     return 0;
 }
 
@@ -176,6 +276,7 @@ void ff_rm_free_rmstream (RMStream *rms)
     if(rms)
         av_freep(&rms);
 }
+
 
 AVInputFormat ff_ra_demuxer = {
     .name           = "ra",
