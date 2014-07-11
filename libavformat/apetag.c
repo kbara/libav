e14651957aefe15b42e8c67fb7d9366a09d1ae52
Katerina Barone-Adesi <katerinab+libav@gmail.com>
2014-07-12 00:01:33 +0200

    Fixed apetag's ape_tag_read_field test.
    The size variable is (correctly) unsigned, but is passed to several functions
    which take signed parameters, such as avio_read and av_malloc, sometimes
    after having numbers added to it.

--- libavformat/apetag.c
+++ libavformat/apetag.c
@@ -57,8 +57,8 @@ static int ape_tag_read_field(AVFormatContext *s)
         av_log(s, AV_LOG_WARNING, "Invalid APE tag key '%s'.\n", key);
         return -1;
     }
-    if (size >= UINT_MAX)
-        return -1;
+    if (size > (unsigned) INT32_MAX - FFMAX(1, FF_INPUT_BUFFER_PADDING_SIZE))
+        return AVERROR_INVALIDDATA;
     if (flags & APE_TAG_FLAG_IS_BINARY) {
         uint8_t filename[1024];
         enum AVCodecID id;
